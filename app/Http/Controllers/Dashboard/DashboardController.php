a5a2ab9e3c0aebae19680878b651bdd8645db482
Adji Maulana Djagad <adjim80@gmail.com>
2020-10-21 21:03:56 +0700

    Add function generateRandomString

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -35,4 +35,15 @@ class DashboardController extends Controller
             echo '<br>';
         }
     }
-}
+
+    public function generateRandomString($length = 152) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    
+}   
